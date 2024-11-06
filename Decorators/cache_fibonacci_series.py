9cea56ee5eef0a02a9426d72ba5329d04ae48a6a
shubham joshi <shubham.joshi1507@gmail.com>
2024-11-06 10:53:46 +0530

    Create cache_fibonacci_series.py
    project using decorator

---
+++ Decorators/cache_fibonacci_series.py
@@ -0,0 +1,32 @@
+#1. Create a decorator called memoize which takes a function as an argument and returns a new function that caches the results of the original function inside a dictionary. wrapper checks if the argument does not exist in dictionary, it adds to cache it.
+
+def memoize(func):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapper
+  
+#2.  Apply  @memoize decorator  to the fibonacci function, which means that whenever fibonacci is called with a particular argument, the decorator will first check if that argument has already been computed and cached. If it has, the cached result is returned instead of recomputing the value.
+
+@memoize
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+#3. Full Code
+
+def memoize(func):
+    cache = {}
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapper
+ 
+@memoize
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
