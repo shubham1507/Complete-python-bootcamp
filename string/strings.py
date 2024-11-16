bd9235896a544e06dd4e66e260c032f6232b39a9
shubham joshi <shubham.joshi1507@gmail.com>
2024-11-16 17:23:29 +0530

    Create strings.py
    string exe

---
+++ string/strings.py
@@ -0,0 +1,33 @@
+# reverse the sentense
+
+string = "geeks quiz practice code"
+
+s=string.split()[::-1]
+l=[]
+for i in s:
+    l.append(i)
+    
+print(" ".join(l))
+
+# remove the white spaces
+
+print("".join(string.split()))
+
+#How to Remove Letters From a String in Python
+
+print(string.replace("g","k"))
+
+#Python program to print even length words in a string
+ even=string.split()
+
+for word in even:
+  if len(word) %2==0:
+    print(word)
+
+#Uppercase Half String
+
+st="".join(string.split())
+half_idx=len(st)//2
+new_string=st[half_idx:]+st[:half_idx].upper()
+
+print(new_string)
