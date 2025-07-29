f314014f1481ddb57aa8607a4f2831c470fae630
shubham joshi <shubham.joshi1507@gmail.com>
2025-07-29 06:07:17 +0530

    Create strings2.py

---
+++ string/strings2.py
@@ -0,0 +1,25 @@
+#length of string
+
+a = "geeks"
+
+print(len(a))
+
+#Reverse Words in a Given String in Python
+s = "Python is a gen lang"
+reversed = ' '.join(s.split()[::-1])
+print(reversed)
+
+#How to Remove Letters From a String in Python
+
+s = "hello world"
+
+rem_ith = s.replace("o","")
+
+print(rem_ith)
+
+#Avoid Spaces in string length
+
+space_avoided = s.replace(" ","")
+print(space_avoided)
+
+
